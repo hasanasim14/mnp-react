8317f3abf760086fd6e0af62c2d08103bb355bda
Hasan Aasim <hasan.asim14@gmail.com>
2025-06-30 11:49:37 +0500

    Preparing to vercel

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -38,7 +38,7 @@ const Message = ({
 
   const fetchExistingMessages = async (sessionId: string) => {
     try {
-      const res = await fetch("api/chatHistory", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/chatHistory`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -146,7 +146,7 @@ const Message = ({
     const timeoutId = setTimeout(() => controller.abort(), timeout);
 
     try {
-      const res = await fetch("api/agent", {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/agent`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
